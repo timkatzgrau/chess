88ca6c1e59e3649daaf6db61886a4472cdc3c89f
Tim Katzgrau <Katzgrau@Tims-Air.fios-router.home>
2017-10-29 06:44:08 -0400

    Knight Completed

--- chess/src/pieces/Knight.java
+++ chess/src/pieces/Knight.java
@@ -1,5 +1,7 @@
 package pieces;
 
+import game.ChessBoard;
+
 public class Knight extends Piece {
 	
 	public Knight (char type, char color) {
@@ -10,8 +12,13 @@ public class Knight extends Piece {
 	}
 
 	@Override
-	public boolean canDoMove(String curr, String fin, Piece[][] board) {
-		// TODO Auto-generated method stub
+	public boolean canDoMove(ChessBoard board, int startColumn, int startRow, int endColumn, int endRow) {
+		if(startColumn+2 == endColumn && startRow+1 == endRow || startColumn-2 == endColumn && startRow+1 == endRow || startColumn+2 == endColumn && startRow-1 == endRow || startColumn-2 == endColumn && startRow-1 == endRow) {
+			return true;
+		}
+		if(startColumn+1 == endColumn && startRow+2 == endRow || startColumn-1 == endColumn && startRow+2 == endRow || startColumn+1 == endColumn && startRow-2 == endRow || startColumn-1 == endColumn && startRow-2 == endRow) {
+			return true;
+		}
 		return false;
 	}
 
