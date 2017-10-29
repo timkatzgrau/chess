eab78bf959797c67935c4a3f8214540a5cd69e7a
Tim Katzgrau <Katzgrau@Tims-Air.fios-router.home>
2017-10-28 21:08:29 -0400

    Fucked up on the last commits lolol

--- chess/src/ChessBoard.java
+++ chess/src/ChessBoard.java
@@ -13,7 +13,98 @@ public class ChessBoard {
 	public Piece[][] chessBoard = new Piece[8][8];
 	
 	public boolean checkmate = false;
+	public boolean whitesTurn = true;
+	public int convertFile (char file) {
+		
+		if (file == 'a') {
+			return 0;
+		} else if (file == 'b') {
+			return 1;
+		} else if (file == 'c') {
+			return 2;
+		} else if (file == 'd') {
+			return 3;
+		} else if (file == 'e') {
+			return 4;
+		} else if (file == 'f') {
+			return 5;
+		} else if (file == 'g') {
+			return 6;
+		} else if (file == 'h') {
+			return 7;
+		}
+		
+		return -1;
+		
+	}
 	
+	public int convertRank (char rank) {
+		
+		if (rank == '8') {
+			return 0;
+		} else if (rank == '7') {
+			return 1;
+		} else if (rank == '6') {
+			return 2;
+		} else if (rank == '5') {
+			return 3;
+		} else if (rank == '4') {
+			return 4;
+		} else if (rank == '3') {
+			return 5;
+		} else if (rank == '2') {
+			return 6;
+		} else if (rank == '1') {
+			return 7;
+		}
+		
+		return -1;
+		
+	}
+	
+	public char convertIndexToFile (int index) {
+		if (index == 0) {
+			return 'a';
+		} else if (index == 1) {
+			return 'b';
+		} else if (index == 2) {
+			return 'c';
+		} else if (index == 3) {
+			return 'd';
+		} else if (index == 4) {
+			return 'e';
+		} else if (index == 5) {
+			return 'f';
+		} else if (index == 6) {
+			return 'g';
+		} else if (index == 7) {
+			return 'h';
+		}
+		
+		return '0';
+	}
+	
+	public char convertIndexToRank (int index) {
+		if (index == 0) {
+			return '8';
+		} else if (index == 1) {
+			return '7';
+		} else if (index == 2) {
+			return '6';
+		} else if (index == 3) {
+			return '5';
+		} else if (index == 4) {
+			return '4';
+		} else if (index == 5) {
+			return '3';
+		} else if (index == 6) {
+			return '2';
+		} else if (index == 7) {
+			return '1';
+		}
+		
+		return '0';
+	}
 	public ChessBoard () {
 		chessBoard[0][0] = new Rook('R', 'b');
 		chessBoard[0][1] = new Knight('N', 'b');
--- chess/src/Game.java
+++ chess/src/Game.java
@@ -1,15 +1,65 @@
-
+import java.util.Scanner;
 
 public class Game {
 	
+	private static Scanner scanner;
+
+	public static boolean attemptMove(ChessBoard board, int startColumn, int startRow, int endColumn, int endRow){		
+		if(board.chessBoard[startRow][startColumn] == null){
+			System.out.println("This is not a valid piece.");
+			return false;
+		}else if(board.chessBoard[startRow][startColumn].color == 'b' && board.whitesTurn == true){
+			System.out.print(board.chessBoard[startRow][startColumn].color);
+			System.out.print(board.whitesTurn);
+			System.out.println("Invalid Move. It is white's turn.");
+			return false;
+		}else if(board.chessBoard[startRow][startColumn].color == 'w' && board.whitesTurn == false) {
+			System.out.println("Invalid Move. It is black's turn.");
+			return false;
+		}
+		return true;
+	}
 	public static void main(String[] args) {
 		ChessBoard board = new ChessBoard();
-		//board.showChessBoard();
-		System.out.println("starting chess game");
-		
-//		while (!board.checkmate) {
-//			System.out.println("playing game");
-//		}
-	}
 
+		while(true) {
+			Scanner scanner = new Scanner(System.in).useDelimiter(" ");
+			board.showChessBoard();
+	
+			if(board.whitesTurn) {
+				System.out.println("White's Turn.");
+			}else {
+				System.out.println("Black's Turn.");
+			}
+			
+			System.out.print("Enter your move: ");
+			String startingPosition = scanner.nextLine();
+			int startingColumn = board.convertFile(startingPosition.charAt(0));
+			int startingRow = board.convertRank(startingPosition.charAt(1));	
+			int endingColumn = board.convertFile(startingPosition.charAt(3));
+			int endingRow =  board.convertRank(startingPosition.charAt(4));	
+			if(startingColumn < 0 || startingRow < 0 ||endingColumn < 0 || endingRow < 0 ||startingColumn > 8 || startingRow > 8 ||endingColumn > 8 || endingRow > 8 ) {
+				System.out.println("One of those positions doesn't exist");
+				System.out.println(startingColumn);
+				System.out.println(startingRow);
+				System.out.println(endingColumn);
+				System.out.println(endingRow);
+				System.out.println(startingPosition);
+			}else {
+				System.out.println(startingColumn);
+				System.out.println(startingRow);
+				System.out.println(endingColumn);
+				System.out.println(endingRow);
+				System.out.println(startingPosition);
+		
+				
+		//		while (!board.checkmate) {
+		//			System.out.println("playing game");
+		//		}
+			}
+			if(attemptMove(board,startingColumn,startingRow, endingColumn, endingRow)) {
+				board.whitesTurn = !board.whitesTurn;
+			}
+		}
+	}
 }
